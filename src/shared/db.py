fb145eea236897cb8086b4c0e8af98da8ba871cc
Joseph Kamau Maina <kamau2589000@gmail.com>
2026-01-04 20:01:54 +0300

    refactor(db): update declarative_base to SQLAlchemy 2.0 style

--- src/shared/db.py
+++ src/shared/db.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine, Column, Integer, String, Float, BigInteger
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 from minio import Minio
 import io
 
